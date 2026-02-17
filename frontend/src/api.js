b280e05c5a3ea905f3c6540752fff25834c6edad
Developer <developer@example.com>
2026-02-17 14:41:11 +0530

    feat(frontend): create Axios API service module

---
+++ frontend/src/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: '/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export const getTickets = (params = {}) => api.get('/tickets/', { params });
+export const createTicket = (data) => api.post('/tickets/', data);
+export const updateTicket = (id, data) => api.patch(`/tickets/${id}/`, data);
+export const getStats = () => api.get('/tickets/stats/');
+export const classifyTicket = (description) =>
+    api.post('/tickets/classify/', { description });
+
+export default api;
